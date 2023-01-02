29405b22eb9c8f4b1086d8f7a83793cd3559b4a3
John J Setlock II <johnsetlock@gmail.com>
2023-01-02 13:43:37 -0600

    progress

--- motion.js
+++ motion.js
@@ -10,27 +10,27 @@ let threshold = 30;
 let scalefactor = 1;
 
 function setup() {
-  //ctx = createCanvas(w, h);
+  ctx = createCanvas(w, h);
   capture = createCapture(VIDEO);
-  //capture.size(w, h);
-  //capture.hide();
-  //ctx.style('scale','400%');
-  //ctx.position(200,200)
+  capture.size(w, h);
+  capture.hide();
+  ctx.style('scale','1000%');
+  ctx.position(480,640)
   //capture.scale(-1,1)
   capture.style('scale','200%')
   //noLoop();
-  frameRate(5);
+  frameRate(1);
 }
 function draw(){
     
     //diff.updatePixels();
     //console.log(prevFrame)
    // ctx.drawImage(VIDEO, 0,0,w,h)
-    // c = capture.get(0,0,w,h);
+     c = capture.get(0,0,w,h);
  
     // push();
     // translate(width,0);
     // scale(-1, 1);
-    // image(c, 0, 0);
+     image(c, 0, 0);
     // pop();
 }
